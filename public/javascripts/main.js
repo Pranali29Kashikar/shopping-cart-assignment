7cf78d15333c571df48cd251e6d829781c33cb04
Pranali Pradip Kashikar <prakashi@publicisgroupe.net>
2019-04-29 13:19:09 +0530

    PR comment & BEM changes for header

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,8 @@
- function myFunction() {
-   var x = document.getElementById("myLinks");
-   if (x.style.display === "block") {
-     x.style.display = "none";
-   } else {
-     x.style.display = "block";
-   }
- }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 function plusSlides(n) {
   showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-  showSlides(slideIndex = n);
 }
 function showSlides(n) {
   var i;
